2998a44ecadba5d82a9aa38f9d0f064379856d7d
yehoshua <y0587828030@gmail.com>
2026-06-23 11:55:20 +0300

    stap 4

--- database.py
+++ database.py
@@ -1,5 +1,16 @@
+#stap 1
 agent = {'name': 'Alpha', 'level': 3, 'active': True}
 print(agent)
+#stap 2
 print(agent["name"])
-level = agent.get("level")
-print(level)
+#stap 3
+# level = agent.get("level")
+# print(level)
+
+print(agent.get("level"))
+print(agent.get("0"))
+
+#stap4
+agent["score"]="95"
+print(agent)
+
