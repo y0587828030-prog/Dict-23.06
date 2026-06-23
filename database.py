83fce823064f4178e7f0500d77c16a3793aa7ed7
yehoshua <y0587828030@gmail.com>
2026-06-23 13:41:16 +0300

    Part 2 task 1

--- database.py
+++ database.py
@@ -1,52 +1,68 @@
+
+# ##Part 1 — Basics: dict CRUD, keys, values, items
+# #stap 1
+# agent = {'name': 'Alpha', 'level': 3, 'active': True}
+# print(agent)
+# #stap 2
+# print(agent["name"])
+
+# #stap 3
+# # level = agent.get("level")
+# # print(level)
+
+# print(agent.get("level"))
+# print(agent.get("0"))
+# # print(agent["0"])
+
+# #stap4
+# agent["score"]= 95
+# print(agent)
+
+# #stap 5
+# agent["level"]=5
+# print(agent)
+
+# #stap 6
+# del(agent["active"])
+# print(agent)
+
+# #stap 7
+# keys = agent.keys()
+# print(keys)
+
+# values = agent.values()
+# print(values)
+
+# items = agent.items()
+# print(items)
+
+# #stap 8
+# if "score" in agent:
+#  print("yes")
+
+#  #stap 9
+# scores = {'Alpha': 80, 'Bravo': 95, 'Charlie': 70}
+# print(scores)
+# print(max(scores.values())) 
+
+# #stap 10
+# copy_agent = agent.copy()
+# print(copy_agent)
+
+# copy_agent["name"]= "yehoshua"
+# print(copy_agent)
+# print(agent)
+
+## OPart 2 — Optional Advanced Basics
+
 #stap 1
-agent = {'name': 'Alpha', 'level': 3, 'active': True}
-print(agent)
-#stap 2
-print(agent["name"])
+config ={}
+print(config)
+# new_key = config.setdefault("timeout" , 30)
+# print(new_key)
 
-#stap 3
-# level = agent.get("level")
-# print(level)
+config.setdefault("timeout" , 30)
+print(config)
+config.setdefault("timeout" , 40)
+print(config)
 
-print(agent.get("level"))
-print(agent.get("0"))
-# print(agent["0"])
-
-#stap4
-agent["score"]= 95
-print(agent)
-
-#stap 5
-agent["level"]=5
-print(agent)
-
-#stap 6
-del(agent["active"])
-print(agent)
-
-#stap 7
-keys = agent.keys()
-print(keys)
-
-values = agent.values()
-print(values)
-
-items = agent.items()
-print(items)
-
-#stap 8
-if "score" in agent:
- print("yes")
-
- #stap 9
-scores = {'Alpha': 80, 'Bravo': 95, 'Charlie': 70}
-print(scores)
-print(max(scores.values())) 
-
-#stap 10
-copy_agent = agent.copy()
-print(copy_agent)
-
-copy_agent["name"]= "yehoshua"
-print(copy_agent)
-print(agent)
